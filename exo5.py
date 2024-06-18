bc65e51c152d4a84cc18197d30b22a050bed853a
Le Hegaret Viviane <attilavlh10@gmail.com>
2024-06-18 22:46:01 +0200

    Update exo5.py

--- exo5.py
+++ exo5.py
@@ -1,1 +1,11 @@
+t1=[31,28,31,30,31,30,31,31,30,31,30,31]
+t2=['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
 
+
+t3=[]
+
+for n in range(len(t1)):
+    t3.append(t2[n])
+    t3.append(t1[n])
+
+print(t2)
